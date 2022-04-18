08ff61362db82e172893d80ae6ec51f2bd5a7416
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-04-18 08:58:32 -0300

    Operadores Aritiméticos

---
+++ src/academy/devdojo/maratonajava/introducao/Operadores.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+// + - / *
+public class Operadores {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        double numero2 = 20;
+        double resultadoMultiplicacao = numero1 + numero2;
+        double resultadodivisao = numero2 / numero1;
+
+        System.out.println(numero1 + numero2);
+        System.out.println("Valor " + numero1 + numero2);
+        System.out.println(numero1 + numero2 + " Valor " + numero1 + numero2);
+        System.out.println(resultadoMultiplicacao);
+        System.out.println(resultadodivisao);
+        System.out.println(numero1 * numero2);
+
+    }
+}
