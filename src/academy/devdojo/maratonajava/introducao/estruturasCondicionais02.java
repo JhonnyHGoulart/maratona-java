082f99a1cc1169d5836b299f75e2b59c145d6b64
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-04-19 09:12:25 -0300

    Estrutura condicional if else

--- src/academy/devdojo/maratonajava/introducao/estruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/estruturasCondicionais02.java
@@ -1,2 +1,20 @@
-package academy.devdojo.maratonajava.introducao;public class estruturasCondicionais02 {
+package academy.devdojo.maratonajava.introducao;
+
+public class estruturasCondicionais02 {
+    public static void main(String[] args) {
+        //idade < 15 categoria infantil
+        //idade >= 15 && <18 categoria juvenil
+        //idade >= 18 categoria adulto
+        int idade = 17;
+        String categoria;
+
+        if (idade < 15) {
+            categoria = "Infaltil";
+        } else if (idade >= 15 && idade < 18) {
+            categoria = "Juvenil";
+        } else {
+            categoria = "Adulto";
+        }
+        System.out.println(categoria);
+    }
 }
