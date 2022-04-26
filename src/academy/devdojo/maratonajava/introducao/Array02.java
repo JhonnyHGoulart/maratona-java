acf6f3ad94a9c61bdf5885e7907184be16ca9a81
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-04-26 08:35:52 -0300

    Arrays pt 02, padrões de inicialização

---
+++ src/academy/devdojo/maratonajava/introducao/Array02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Array02 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        //Padrão de inicialização
+        //byt, short, int, long, float, double == 0
+        //char == '\ u000' "Espaço em branco"
+        //boolean == false
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
