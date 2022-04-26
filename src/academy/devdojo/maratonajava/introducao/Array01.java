b853ca2a48846ad492161e6cf361c1daf2660a26
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-04-26 08:11:06 -0300

    Arrays pt 01

---
+++ src/academy/devdojo/maratonajava/introducao/Array01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Array01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 1;
+        idades[1] = 2;
+        idades[2] = 3;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
