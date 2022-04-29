6855a9285bb3d6411418151417da142c15277a53
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-04-29 15:26:15 -0300

    Primeiro método do curso, criando um método void para calcular 2 números

---
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoreTest.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.introducaometodos.model.Calculadora;
+
+public class CalculadoreTest {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somarDoisNumeros();
+        calculadora.subtrairDoisNumeros();
+
+    }
+}
