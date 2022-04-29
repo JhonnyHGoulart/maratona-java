deb2517fe94a22e8f6d78eb5f186d3509c9959c6
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-04-29 08:45:05 -0300

    Orientação a Objetos, instanciando um objeto e inserindo valores a os atributos

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.model.Estudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "jhonny";
+        estudante.idade = 28;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+    }
+}
