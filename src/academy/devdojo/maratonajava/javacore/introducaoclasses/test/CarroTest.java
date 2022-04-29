5947fe69dc22fadb95db62cae47fb3bbb06abd8d
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-04-29 13:47:26 -0300

    Orientação a Objetos, Exercício de classes

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/CarroTest.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.model.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Carro carro2 = new Carro();
+
+        carro.nome = "Fusca";
+        carro.ano = 1965;
+        carro.modelo = "Fusca Bala";
+
+        carro2.nome = "Mustang";
+        carro2.ano = 1968;
+        carro2.modelo = "GT 508";
+
+        System.out.println("Carro 1 \nNome: " + carro.nome + " \nAno: " + carro.ano + " \nModelo: " + carro.modelo);
+        System.out.println("\nCarro 1 \nNome: " + carro2.nome + " \nAno: " + carro2.ano + " \nModelo: " + carro2.modelo);
+    }
+}
