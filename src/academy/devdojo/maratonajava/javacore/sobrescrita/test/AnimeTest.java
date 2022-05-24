2c0b3ff80a91e8c9f42b7af570e780df445d33e4
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-05-24 08:45:47 -0300

    Sobrescrita, sobrescrevendo o método toString

---
+++ src/academy/devdojo/maratonajava/javacore/sobrescrita/test/AnimeTest.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.sobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.sobrescrita.model.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
