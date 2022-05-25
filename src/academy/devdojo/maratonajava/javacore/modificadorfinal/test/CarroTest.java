41e8f2da1e65b1aae45beca98fe3a82746899e7e
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-05-25 08:11:22 -0300

    Modificador final para tipos primitivos

---
+++ src/academy/devdojo/maratonajava/javacore/modificadorfinal/test/CarroTest.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.modificadorfinal.test;
+
+import academy.devdojo.maratonajava.javacore.modificadorfinal.model.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
