58370475e8b8a2607614749134d47dd4e8c0b190
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-05-25 08:43:33 -0300

    Modificador final para tipo referência

--- src/academy/devdojo/maratonajava/javacore/modificadorfinal/test/CarroTest.java
+++ src/academy/devdojo/maratonajava/javacore/modificadorfinal/test/CarroTest.java
@@ -6,5 +6,8 @@ public class CarroTest {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Mateus");
+        System.out.println(carro.COMPRADOR);
     }
 }
