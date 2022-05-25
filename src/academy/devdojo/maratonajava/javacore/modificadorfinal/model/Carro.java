8a47355c3c8718448022bc7a8f5fb39fe253226d
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-05-25 09:05:40 -0300

    Modificador final para classes e métodos

--- src/academy/devdojo/maratonajava/javacore/modificadorfinal/model/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/modificadorfinal/model/Carro.java
@@ -1,9 +1,21 @@
 package academy.devdojo.maratonajava.javacore.modificadorfinal.model;
 
-public class Carro {
+/**
+ * Qualquer classe com o modificador final não pode ser estendida.
+ */
+public final class Carro {
     private String nome;
     public static final double VELOCIDADE_LIMITE = 250;
     public final Comprador COMPRADOR = new Comprador();
+
+    /**
+     * Qualquer método com o modificador final não pode ser sobrescrito.
+     * Criar um método com o modificador fina em uma classe que também é final, é redundante, pois se não pode estender
+     * a classe também não pode sobrescrever seus métodos.
+     */
+    public final void imprime(){
+        System.out.println(this.nome);
+    }
 
     public String getNome() {
         return nome;
