1e802180eef37005d1b14a5845690717d1019fbb
Jhonny Hilário <jhonny.goulart@sc.senai.br>
2022-06-02 08:51:14 -0300

    Enumeração - Sobrescrevendo um método diretamente na enumeração

--- src/academy/devdojo/maratonajava/javacore/enumeracao/test/ClenteTest.java
+++ src/academy/devdojo/maratonajava/javacore/enumeracao/test/ClenteTest.java
@@ -2,15 +2,18 @@ package academy.devdojo.maratonajava.javacore.enumeracao.test;
 
 import academy.devdojo.maratonajava.javacore.enumeracao.model.Cliente;
 import academy.devdojo.maratonajava.javacore.enumeracao.model.TipoCliente;
+import academy.devdojo.maratonajava.javacore.enumeracao.model.TipoPagamento;
 
 public class ClenteTest {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Jhonny", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Jhonny", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("Jhonny", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Jhonny", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
 
     }
 }
